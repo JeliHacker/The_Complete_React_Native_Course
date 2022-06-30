3db254e3c806104d4a2d045c0206ff9ebeec2e19
JeliHacker <jeligooch@gmail.com>
2022-06-29 17:31:56 -0700

    hopefully through 133

---
+++ blog/src/context/BlogContext.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const BlogContext = React.createContext();
+
+export const BlogProvider = ({ children }) => {
+  const blogPosts = [
+    { title: 'Blog Post #1' },
+    { title: 'Blog Post #2' }
+  ];
+
+  return (
+    // <Text>hello world</Text>
+    <BlogContext.Provider value={blogPosts}>
+      {children}
+    </BlogContext.Provider>
+  );
+};
+
+export default BlogContext;
