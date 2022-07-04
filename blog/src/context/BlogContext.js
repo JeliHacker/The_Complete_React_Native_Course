f972b712b2ba5e7e5991126bae89ba99ea20f392
JeliHacker <jeligooch@gmail.com>
2022-07-03 23:42:48 -0700

    works thru 133

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,13 +1,9 @@
 import React from 'react';
-<<<<<<< HEAD
 import { View, Text, StyleSheet } from 'react-native';
-=======
->>>>>>> 3c392ed4862fa44422e68d9d165562dfac2b9511
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-<<<<<<< HEAD
   const blogPosts = [
     { title: 'Blog Post #1' },
     { title: 'Blog Post #2' }
@@ -19,11 +15,6 @@ export const BlogProvider = ({ children }) => {
       {children}
     </BlogContext.Provider>
   );
-=======
-  return <BlogContext.Provider value={[1, 2, 3]}>
-    {children}
-  </BlogContext.Provider>;
->>>>>>> 3c392ed4862fa44422e68d9d165562dfac2b9511
 };
 
 export default BlogContext;
