1cb881a198e01d8595bf74da9a388354f15b6a10
JeliHacker <jeligooch@gmail.com>
2022-07-06 02:10:55 -0700

    through video 143

--- blog/App.js
+++ blog/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import IndexScreen from "./src/screens/IndexScreen";
-import { BlogProvider } from './src/context/BlogContext.js';
+import { Provider } from './src/context/BlogContext.js';
+import ShowScreen from './src/screens/ShowScreen';
 
 const navigator = createStackNavigator(
   {
-  Index: IndexScreen
+    Index: IndexScreen,
+    Show: ShowScreen
   }, 
   {
     initialRouteName: 'Index',
@@ -20,8 +22,8 @@ const App = createAppContainer(navigator);
 
 export default () => {
   return (
-  <BlogProvider>
+  <Provider>
     <App />
-  </BlogProvider>
+  </Provider>
   );
 };
